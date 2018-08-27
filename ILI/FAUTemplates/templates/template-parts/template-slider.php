e6f7a6257abaaa60a2aed810e4263b86a442b85a
Sebastian Honert <sebastian.honert@gmail.com>
2018-08-27 14:50:14 +0200

    Slider Neta / CSS

--- ILI/FAUTemplates/templates/template-parts/template-slider.php
+++ ILI/FAUTemplates/templates/template-parts/template-slider.php
@@ -4,10 +4,28 @@
  * Template Part for pages with big hero slider
  */
 
-?>
+$ilifautpl_meta = get_post_meta( get_the_ID(), '_ilifautpl_slides', true );
 
-<div class="slick-slider">
-    <div class="slick-slide" style="background:url(<?php echo plugins_url('ili-fau-templates-master/assets/img/1.jpg'); ?>) center center;"></div>
-    <div class="slick-slide" style="background:url(<?php echo plugins_url('ili-fau-templates-master/assets/img/2.jpg'); ?>) center center;"></div>
-    <div class="slick-slide" style="background:url(<?php echo plugins_url('ili-fau-templates-master/assets/img/3.jpg'); ?>) center center;"></div>
-</div>
+if( is_array( $ilifautpl_meta ) && ! empty( $ilifautpl_meta ) ):
+    echo '<div class="slick-slider">';
+        foreach( $ilifautpl_meta as $key => $slide ):
+            $link_html = ! empty( $ilifautpl_meta[$key]['link'] ) ? '<a href="#">' . __('Zum Artikel', 'ilifautpl') . '</a>' : '';
+            $ilifautpl_headline = ! empty( $ilifautpl_meta[$key]['link'] ) ? '<a href="#">' . $ilifautpl_meta[$key]['headline'] . '</a>' : $ilifautpl_meta[$key]['headline'];
+            
+            echo '<div class="slick-slide" style="background:url(' . $ilifautpl_meta[$key]['url'] . ') center center;">';
+                echo '<div class="content">';
+                    echo '<div class="container">';
+                        echo '<div class="row">';
+                            echo '<div class="ilifautpl-slider-content">';
+                                echo '<h2 class="ilifautpl-slider-headline">' . $ilifautpl_headline . '</h2><br />';
+                                echo '<h3 class="ilifautpl-slider-subtitle">' . $ilifautpl_meta[$key]['subtitle'] . '<span class="ilifautpl-slide-read-more">' . $link_html . '</span></h3>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+            echo '</div>';
+        endforeach;
+    echo '</div>';
+endif;
+
+?>
