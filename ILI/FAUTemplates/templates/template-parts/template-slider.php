c570ae6a9110015ba33d50ebe84ba6182dd6f7cd
Sebastian Honert <sebastian.honert@gmail.com>
2018-08-29 16:03:35 +0200

    Topic Boxes CSS Grid

--- ILI/FAUTemplates/templates/template-parts/template-slider.php
+++ ILI/FAUTemplates/templates/template-parts/template-slider.php
@@ -9,15 +9,15 @@ $ilifautpl_meta = get_post_meta( get_the_ID(), '_ilifautpl_slides', true );
 if( is_array( $ilifautpl_meta ) && ! empty( $ilifautpl_meta ) ):
     echo '<div class="slick-slider">';
         foreach( $ilifautpl_meta as $key => $slide ):
-            $link_html = ! empty( $ilifautpl_meta[$key]['link'] ) ? ' <a href="#">' . __('Zum Inhalt', 'ilifautpl') . '</a>' : '';
-            $ilifautpl_headline = ! empty( $ilifautpl_meta[$key]['link'] ) ? '<a href="#">' . $ilifautpl_meta[$key]['headline'] . '</a>' : $ilifautpl_meta[$key]['headline'];
+            $link_html = ! empty( $ilifautpl_meta[$key]['link'] ) ? ' <a href="' . $ilifautpl_meta[$key]['link'] . '">' . __('Zum Inhalt', 'ilifautpl') . '</a>' : '';
+            $ilifautpl_headline = ! empty( $ilifautpl_meta[$key]['link'] ) ? '<a href="' . $ilifautpl_meta[$key]['link'] . '">' . $ilifautpl_meta[$key]['headline'] . '</a>' : $ilifautpl_meta[$key]['headline'];
             
             echo '<div class="slick-slide" style="background:url(' . $ilifautpl_meta[$key]['url'] . ') center center;">';
                 echo '<div class="content">';
                     echo '<div class="container">';
                         echo '<div class="row">';
                             echo '<div class="ilifautpl-slider-content">';
-                                echo '<h2 class="ilifautpl-slider-headline">' . $ilifautpl_headline . '</h2><br />';
+                                echo '<h2 class="ilifautpl-slider-headline"><a href="' . $ilifautpl_meta[$key]['link'] . '">' . $ilifautpl_headline . '</a></h2><br />';
                                 echo '<h3 class="ilifautpl-slider-subtitle">' . $ilifautpl_meta[$key]['subtitle'] . '<span class="ilifautpl-slide-read-more">' . $link_html . '</span></h3>';
                             echo '</div>';
                         echo '</div>';
--- ILI/FAUTemplates/templates/template-parts/template-topic-boxes.php
+++ ILI/FAUTemplates/templates/template-parts/template-topic-boxes.php
@@ -9,8 +9,7 @@ $ilifautpl_selected_topic_boxes = get_post_meta( get_the_ID(), '_ilifautpl_topic
 
 $ilifautpl_topic_boxes = get_posts(array(
     'post_type' => 'ilifautpl_topic_box',
-    'num_posts' => -1,
-    'hide_empty' => true,
+    'numberposts' => -1,
     'include' => $ilifautpl_selected_topic_boxes,
 ));
 
@@ -19,37 +18,25 @@ $ilifautpl_topix_box_excerpt_length = 230;
 echo '<div class="content">';
     echo '<div class="container">';
         echo '<div class="row">';
-            $ilifautpl_count_boxes = 1;
-            
-            foreach( $ilifautpl_topic_boxes as $box ) {
-                $ilifautpl_topic_box_target_id = get_post_meta( $box->ID, '_ilifautpl_topic_box_target_id', true );
-                
-                if( ! $ilifautpl_topic_box_target_id )
-                    continue;
-                
-                if( $ilifautpl_count_boxes === 1 )
-                    echo '<div class="ilifautpl-topic-box-row">';
-
-                $ilifautpl_topix_box_url = esc_url( get_permalink( $ilifautpl_topic_box_target_id ) );
-                $ilifautpl_topic_box_excerpt = strlen( $box->post_content ) >= $ilifautpl_topix_box_excerpt_length ? substr( $box->post_content, 0, $ilifautpl_topix_box_excerpt_length ) . '&hellip; <a class="ilifautpl-topix-box-read-more" href="' . $ilifautpl_topix_box_url . '">' . __('Weiter lesen', 'ilifautpl') . '</a>' : $box->post_content;
-                
-                echo '<div class="ilifautpl-topic-box">';
-                    echo '<a href="' . $ilifautpl_topix_box_url . '">';
-                        echo get_the_post_thumbnail( $box->ID, 'ilifautpl-topic-box', array( 'class' => 'ilifautpl-topic-box-image' ) );
-                        echo '<h3 class="ilifautpl-topic-box-title">' . $box->post_title . '</h3>';
-                    echo '</a>';
-                    echo '<p class="ilifautpl-topic-box-content">' . $ilifautpl_topic_box_excerpt . '</p>';
-                echo '</div>';
-                
-                if( $ilifautpl_count_boxes % 3 === 0 ) {
+            echo '<div class="ilifautpl-topic-boxes">';
+                foreach( $ilifautpl_topic_boxes as $box ) {
+                    $ilifautpl_topic_box_target_id = get_post_meta( $box->ID, '_ilifautpl_topic_box_target_id', true );
+                    
+                    if( ! $ilifautpl_topic_box_target_id )
+                        continue;
+                        
+                    $ilifautpl_topix_box_url = esc_url( get_permalink( $ilifautpl_topic_box_target_id ) );
+                    $ilifautpl_topic_box_excerpt = preg_replace('/\s+?(\S+)?$/', '', substr($box->post_content, 0, $ilifautpl_topix_box_excerpt_length)) . '&hellip;';
+                    
+                    echo '<div class="ilifautpl-topic-box">';
+                        echo '<a href="' . $ilifautpl_topix_box_url . '">';
+                            echo get_the_post_thumbnail( $box->ID, 'ilifautpl-topic-box', array( 'class' => 'ilifautpl-topic-box-image' ) );
+                            echo '<h3 class="ilifautpl-topic-box-title">' . $box->post_title . '</h3>';
+                        echo '</a>';
+                        echo '<p class="ilifautpl-topic-box-content">' . $ilifautpl_topic_box_excerpt . ' <a class="ilifautpl-topix-box-read-more" href="' . $ilifautpl_topix_box_url . '">' . __('Weiter lesen', 'ilifautpl') . '</a></p>';
                     echo '</div>';
-                    $ilifautpl_count_boxes = 1;
-                } else {
-                    $ilifautpl_count_boxes++;
-                }    
-
-                
-            }
+                }
+                echo '</div>';
         echo '</div>';
     echo '</div>';
 echo '</div>';
