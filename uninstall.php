51c8d52403e7aa4b2afeafc0f7a6baf47ba85a3b
Sebastian Honert <sebastian.honert@gmail.com>
2018-09-03 15:46:18 +0200

    Uninstall

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+
+if( ! defined('WP_UNINSTALL_PLUGIN') ) {
+    die;
+}
+
+// Delete options
+$option_name = 'ili_fau_templates';
+
+delete_option($option_name);
+delete_site_option($option_name); // Multisite
+
+// Delete topic boxes
+$args = array(
+  'post_type' => 'ilifautpl_topic_box',
+  'nopaging' => true,
+);
+
+$query = new WP_Query ($args);
+
+while( $query->have_posts () ) {
+  $query->the_post ();
+  $id = get_the_ID ();
+  wp_delete_post ($id, true);
+}
