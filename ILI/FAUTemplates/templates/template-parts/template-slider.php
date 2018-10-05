806e71c5c9415859ac2e750d3691ec3cdf7aab86
Sebastian Honert <sebastian.honert@gmail.com>
2018-10-05 16:06:37 +0200

    Fix Slider

--- ILI/FAUTemplates/templates/template-parts/template-slider.php
+++ ILI/FAUTemplates/templates/template-parts/template-slider.php
@@ -22,7 +22,7 @@ if( is_array( $ilifautpl_meta ) && ! empty( $ilifautpl_meta ) ):
                 echo '<div class="container">';
                     echo '<div class="row">';
                         echo '<div class="ilifautpl-slider-content">';
-                            echo '<h3><a href="' . $ilifautpl_meta[$key]['link'] . '">' . $ilifautpl_headline . '</a></h3><br />';
+                            echo '<h3><a href="' . $ilifautpl_meta[$key]['link'] . '">' . $ilifautpl_headline . '</a></h3>';
                             echo '<p>' . $ilifautpl_meta[$key]['subtitle'] . '<span class="ilifautpl-slide-read-more">' . $link_html . '</span></p>';
                         echo '</div>';
                     echo '</div>';
